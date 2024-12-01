b293a6d4bbe9647e5851cb80d96add33f3617260
Deniz Sarici <deniz.sarici@gmail.com>
2024-12-01 09:00:12 +0100

    do part 2

--- 2024/src/main/java/aoc/Day01.java
+++ 2024/src/main/java/aoc/Day01.java
@@ -3,7 +3,9 @@ package aoc;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import one.util.streamex.StreamEx;
 
@@ -11,15 +13,26 @@ public class Day01 {
     record Pair(int left, int right){}
 
     public static void main(String[] args) throws IOException {
+
+        // Part 1
         List<Pair> input = read();
 
         List<Integer> left = input.stream().map(Pair::left).sorted().toList();
         List<Integer> right = input.stream().map(Pair::right).sorted().toList();
 
-        int result = StreamEx.zip(left, right, (l, r) -> Math.abs(l - r))
+        int resultP1 = StreamEx.zip(left, right, (l, r) -> Math.abs(l - r))
             .mapToInt(i -> (int)i)
             .sum();
-        System.out.println(result);
+        System.out.println(resultP1);
+
+        // Part 2
+        Map<Integer, Integer> countsRight = new HashMap<>();
+        right.forEach(num -> countsRight.merge(num, 1, Integer::sum));
+
+        int resultP2 = left.stream()
+            .mapToInt(num -> num *= countsRight.getOrDefault(num, 0))
+            .sum();
+        System.out.println(resultP2);
     }
 
     private static List<Pair> read() throws IOException {
