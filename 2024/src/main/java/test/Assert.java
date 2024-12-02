1d71b4a8d50eeebee3e25c64a54e9dd1c0480d75
deniz sarici <deniz.sarici@zkb.ch>
2024-12-02 13:46:29 +0100

    assert

--- 2024/src/main/java/test/Assert.java
+++ 2024/src/main/java/test/Assert.java
@@ -2,7 +2,13 @@ package test;
 
 
 public class Assert {
-    
+
+    public static void assertTrue(boolean b) {
+      if (!b) {
+        throw new AssertionError();
+      }
+    }
+
     public static void assertTrue(boolean b, String msg) {
         if (!b) {
             throw new AssertionError(msg);
