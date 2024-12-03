7062ce98891abd7a6d7171e7de9c40887df725a2
deniz sarici <deniz.sarici@zkb.ch>
2024-12-03 08:57:14 +0100

    Day 3 -- Part 2

--- 2024/src/main/java/test/Assert.java
+++ 2024/src/main/java/test/Assert.java
@@ -15,6 +15,10 @@ public final class Assert {
     }
   }
 
+  public static void assertEquals(long expected, long actual) {
+    assertEquals(expected, actual, "");
+  }
+
   public static void assertEquals(long expected, long actual, String part) {
     if (expected != actual) {
       throw new AssertionError(part + ": Expected " + expected + " but was " + actual);
