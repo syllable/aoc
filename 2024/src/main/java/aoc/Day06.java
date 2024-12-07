5bf982c03d366cd6033e0ee426ea4c441bca6fb3
Deniz Sarici <deniz.sarici@gmail.com>
2024-12-07 12:57:42 +0100

    organize imports

--- 2024/src/main/java/aoc/Day06.java
+++ 2024/src/main/java/aoc/Day06.java
@@ -7,11 +7,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.StructuredTaskScope;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.LongAdder;
 
 import static test.Assert.assertEquals;
 
